4fd17f3cf4cc286f677651a959df1b6b91228596
Alexis Aguilera <aguileraalexis@yahoo.com>
2025-12-31 18:39:17 -0500

    Infraestructura - correcion pipeline

--- infra/14-API-Gateway.tf
+++ infra/14-API-Gateway.tf
@@ -1,46 +1,24 @@
 ############################
-# API Gateway HTTP API + VPC Link + Proxy ($default)
+# API Gateway HTTP API -> ALB public (no VPC Link)
 ############################
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "${local.name_prefix}-http-api"
   protocol_type = "HTTP"
 }
 
-# SG para VPC Link: permite egress al ALB (80)
-resource "aws_security_group" "vpclink_sg" {
-  name        = "${local.name_prefix}-vpclink-sg"
-  description = "API GW VPC Link egress to ALB"
-  vpc_id      = data.aws_vpc.default.id
-
-  egress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-}
-
-resource "aws_apigatewayv2_vpc_link" "vpclink" {
-  name               = "${local.name_prefix}-vpclink"
-  subnet_ids         = local.subnet_ids
-  security_group_ids = [aws_security_group.vpclink_sg.id]
-}
-
-# Integración proxy hacia ALB Listener
 resource "aws_apigatewayv2_integration" "alb_proxy" {
   api_id                 = aws_apigatewayv2_api.http_api.id
   integration_type       = "HTTP_PROXY"
   integration_method     = "ANY"
-  connection_type        = "VPC_LINK"
-  connection_id          = aws_apigatewayv2_vpc_link.vpclink.id
-  integration_uri        = aws_lb_listener.http.arn
   payload_format_version = "1.0"
+
+  # Proxy completo hacia ALB (mantiene path)
+  integration_uri = "http://${aws_lb.app.dns_name}/{proxy}"
 }
 
-# $default = proxy total (incluye /actuator/**)
-resource "aws_apigatewayv2_route" "default" {
+resource "aws_apigatewayv2_route" "proxy" {
   api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "$default"
+  route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.alb_proxy.id}"
 }
 
@@ -50,4 +28,6 @@ resource "aws_apigatewayv2_stage" "default" {
   auto_deploy = true
 }
 
-
+output "api_gateway_url" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
