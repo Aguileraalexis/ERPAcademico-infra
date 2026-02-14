a6321df6b740727c9c98e2ddcb470ac1dda73155
Alexis Aguilera <aguileraalexis@yahoo.com>
2026-02-14 10:57:20 -0500

    bucket simple para archivos

--- infra/08-S3.tf
+++ infra/08-S3.tf
@@ -3,19 +3,11 @@ resource "aws_s3_bucket" "bkt-lambdas" {
   bucket = "${local.name_prefix}-lambdas"
 }
 
-resource "aws_s3_bucket" "bkt-fotos" {
-  bucket = "${local.name_prefix}-fotos"
+resource "aws_s3_bucket" "bkt_archivos" {
+  bucket = "${local.name_prefix}-archivos-${var.spring_env["SPRING_PROFILES_ACTIVE"]}"
 }
 
-resource "aws_s3_bucket" "bkt-resumen-matricula" {
-  bucket = "${local.name_prefix}-resumen-matricula"
-}
-
-resource "aws_s3_bucket" "bkt-cert-preinscripcion" {
-  bucket = "${local.name_prefix}-cert-preinscripcion"
-}
-
-resource "aws_s3_bucket" "bkt-cert-matricula" {
-  bucket = "${local.name_prefix}-cert-matricula"
+resource "aws_s3_bucket" "bkt_local" {
+  bucket = "${local.name_prefix}-archivos-local"
 }
 
--- infra/14-API-Gateway.tf
+++ infra/14-API-Gateway.tf
@@ -1,33 +1,33 @@
-############################
-# API Gateway HTTP API -> ALB public (no VPC Link)
-############################
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "${local.name_prefix}-http-api"
-  protocol_type = "HTTP"
-}
+  ############################
+  # API Gateway HTTP API -> ALB public (no VPC Link)
+  ############################
+  resource "aws_apigatewayv2_api" "http_api" {
+    name          = "${local.name_prefix}-http-api"
+    protocol_type = "HTTP"
+  }
 
-resource "aws_apigatewayv2_integration" "alb_proxy" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "HTTP_PROXY"
-  integration_method     = "ANY"
-  payload_format_version = "1.0"
+  resource "aws_apigatewayv2_integration" "alb_proxy" {
+    api_id                 = aws_apigatewayv2_api.http_api.id
+    integration_type       = "HTTP_PROXY"
+    integration_method     = "ANY"
+    payload_format_version = "1.0"
 
-  # Proxy completo hacia ALB (mantiene path)
-  integration_uri = "http://${aws_lb.app.dns_name}/{proxy}"
-}
+    # Proxy completo hacia ALB (mantiene path)
+    integration_uri = "http://${aws_lb.app.dns_name}/{proxy}"
+  }
 
-resource "aws_apigatewayv2_route" "proxy" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.alb_proxy.id}"
-}
+  resource "aws_apigatewayv2_route" "proxy" {
+    api_id    = aws_apigatewayv2_api.http_api.id
+    route_key = "ANY /{proxy+}"
+    target    = "integrations/${aws_apigatewayv2_integration.alb_proxy.id}"
+  }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
+  resource "aws_apigatewayv2_stage" "default" {
+    api_id      = aws_apigatewayv2_api.http_api.id
+    name        = "$default"
+    auto_deploy = true
+  }
 
-output "api_gateway_url" {
-  value = aws_apigatewayv2_api.http_api.api_endpoint
-}
+  output "api_gateway_url" {
+    value = aws_apigatewayv2_api.http_api.api_endpoint
+  }
